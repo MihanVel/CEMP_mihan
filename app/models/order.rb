afb0bab245342159009d565ac175c6a7135b3a27
Mihan <mihanb4r@gmail.com>
2023-09-09 14:22:11 +0200

    after all validation messages added in order form

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,8 +1,10 @@
 class Order < ApplicationRecord
     validates :name, presence: true, length: { minimum:5, maximum: 100}
-    validates :condition, presence: true, length: { minimum:5, maximum: 100}
+    validates :condition, presence: true
     validates :about, presence: true, length: { minimum:5, maximum: 250}
     validates :description, presence: true, length: { minimum:5, maximum: 250}
     validates :quantity, presence: true, numericality: { greater_than: 0 }
     validates :price, presence: true
+    validates :product_size, presence: true
+    validates :prouct_weight, presence: true
 end
